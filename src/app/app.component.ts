7af0c54daadfaf8c6c2dc7dc2854e75e6b3ac012
Jamesholloway28 <Jamesholloway28@gmail.com>
2024-12-17 06:42:35 -0500

    idk

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,21 +4,32 @@ import { Course } from './model/course';
 
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css'],
-    standalone: false
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  standalone: false
 })
 export class AppComponent {
 
-    courses = COURSES;
-    coreCourse = COURSES[0];
-    rxjsCourse = COURSES[1];
-    ngrxCourse = COURSES[2];
+  courses = COURSES;
+  coreCourse = COURSES[0];
+  rxjsCourse = COURSES[1];
+  ngrxCourse = COURSES[2];
 
-    onCourseSelected(course: Course) {
-        console.log('the course card was clicked', course);
+  onCourseSelected(course: Course) {
+    console.log('the course card was clicked', course);
 
-    }
+  }
 
+  data = {
+    title: 'Angular Core Deep Dive'
+  }
+  onLogoClicked() {
+    alert("Hello World!")
+  }
+
+  onKeyUp(newTitle: string) {
+    // console.log(this);
+    this.data.title = newTitle;
+  }
 }
