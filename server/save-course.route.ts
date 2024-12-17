bb17d55ce42f81a70018039ab8f43f6291cd7841
Jamesholloway28 <Jamesholloway28@gmail.com>
2024-12-17 07:16:32 -0500

    yep

--- server/save-course.route.ts
+++ server/save-course.route.ts
@@ -1,18 +1,20 @@
-import {Request, Response} from 'express';
-import {findCourseById} from '../src/db-data';
-
+import { Request, Response } from 'express';
+import { findCourseById } from '../src/db-data';
 
 export function saveCourse(req: Request, res: Response) {
-
-    const id = req.params["id"],
-          changes = req.body;
+    const id = Number(req.params["id"]); // Convert id to a number
+    const changes = req.body;
 
     console.log("Saving course", id, JSON.stringify(changes));
 
     const course = findCourseById(id);
 
+    if (!course) {
+        res.status(404).json({ message: "Course not found" });
+        return;
+    }
+
     course.description = changes.description;
 
     res.status(200).json(course);
-
 }
