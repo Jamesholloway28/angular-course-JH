9a42a025fe5861fad251954b71713b55dd4fb318
Jamesholloway28 <Jamesholloway28@gmail.com>
2024-12-18 13:40:16 -0500

    added course-image component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, QueryList, ViewChild, ViewChildren, viewChildren } from '@angular/core';
 import { COURSES } from '../db-data';
 import { Course } from './model/course';
-
+import { CourseCardComponent } from './course-card/course-card.component';
 
 @Component({
   selector: 'app-root',
@@ -9,13 +9,76 @@ import { Course } from './model/course';
   styleUrls: ['./app.component.css'],
   standalone: false
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
 
-  courses = COURSES;
+  courses = [...COURSES];
 
-  onCourseSelected(course: Course) {
-    console.log('the course card was clicked', course);
+
+  title = COURSES[0].description;
+
+  startDate = new Date(2000, 0, 1)
+
+  price = 9.9953674345;
+
+  rate = .67;
+
+  course0 = COURSES[0];
+
+
+  //// view child decorator:
+  //// CourseCardComponent class from the course-card.component.ts file
+  //// gets just the first matching instance
+  // @ViewChild('cardRef1', { read: ElementRef })
+  // card1: ElementRef;
+
+  // @ViewChild('container')
+  // containerDiv: ElementRef;
+
+  @ViewChildren(CourseCardComponent)
+  cards: QueryList<CourseCardComponent>;
+
+  constructor() {
+    // console.log('containerDiv', this.card1);
+
   }
+
+  ngAfterViewInit() {
+    // console.log(this.cards);
+    //monitors for changes to the collection
+    console.log(this.courses);
+    this.cards.changes.subscribe(
+      cards => console.log(cards)
+    );
+  }
+
+  onCoursesEdited() {
+    this.courses.push(
+      {
+        id: 1,
+        description: "Angular core deep dive",
+        iconUrl: 'https://s3-us-west-1.amazonaws.com/angular-university/course-images/angular-core-in-depth-small.png',
+        longDescription: "A detailed walk-through of the most important part of Angular - the Core and Common modules",
+        category: 'INTERMEDIATE',
+        lessonsCount: 10
+      }
+    );
+  }
+  onCourseSelected(course: Course) {
+    // console.log('containerDiv', this.card1);
+
+    //logs whichever course card button is clicked, handled here in the app.component level is ideal, 
+    // though we could have done it in course-card:
+    console.log(course);
+
+  }
+
+
+  trackCourse(index: number, course: Course): string {
+    console.log(index, course.id);
+
+    return `${index}-${course.id}`
+  }
+
 
   // data = {
   //   title: 'Angular Core Deep Dive'
--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,34 +1,71 @@
-import { Component, Input, Output } from '@angular/core';
+import { AfterViewInit, Component, ContentChild, Input, Output } from '@angular/core';
 import { Course } from '../model/course';
 import { EventEmitter } from '@angular/core';
-
+import { CommonModule,/*NgClass, NgIf*/ } from '@angular/common';
+import { CourseImageComponent } from '../course-image/course-image.component';
 
 @Component({
   selector: 'course-card',
-  imports: [],
+  imports: [CommonModule, /*NgClass, NgIf*/],
   templateUrl: './course-card.component.html',
   styleUrl: './course-card.component.css'
 })
-export class CourseCardComponent {
+export class CourseCardComponent implements AfterViewInit {
+  @Output()
+  courseSelected = new EventEmitter<Course>();
 
   @Input({ required: true })
   course: Course;
 
+  //commented out for single course card example -->
   @Input({ required: true })
   index: number;
 
+  //can only see top part of the content part of the component instance in question
+  @ContentChild(CourseImageComponent)
+  image;
+
+  // //for ngFor
+  // @Input()
+  // cardIndex: number;
 
 
-  @Output()
-  courseSelected = new EventEmitter<Course>();
+  // // for ngIf
+  // isImageVisible() {
+  //   return this.course && this.course.iconUrl;
+  // }
+  ngAfterViewInit(): void {
+    console.log(this.image);
 
-  onClick() {
-    alert("test!")
   }
+
   onCourseViewed() {
     console.log("Card compenent-button clicked...");
+
     this.courseSelected.emit(this.course);
   }
+
+  cardClasses() {
+    if (this.course.category == 'BEGINNER') {
+      return ['beginner'];
+    }
+    else if (this.course.category == 'INTERMEDIATE') {
+      return ['intermediate']
+    } else {
+      return ['advanced']
+
+    }
+  }
+  //ngStyle ----want to use this sparingly
+  cardStyles() {
+    return { 'text-decoration': 'underline', 'font-weight': '600' }
+
+
+  };
 }
 
+
+function ViewContent(arg0: string): (target: CourseCardComponent, propertyKey: "image") => void {
+  throw new Error('Function not implemented.');
+}
 
